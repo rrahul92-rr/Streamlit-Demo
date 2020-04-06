11aa782a48d38282d4306b879aaabeadd3c4699f
Rahul Ramachandran <rrahul92.rr@gmail.com>
2020-04-06 20:37:33 +0530

    Added a couple of functions

--- Streamlit demo.py
+++ Streamlit demo.py
@@ -1,4 +1,22 @@
 import streamlit as st
-st.write("Hello world")
+import requests
+import json
+response = requests.get("https://api.covid19india.org/data.json")
+print(response.json())
+
+
+def jprint(obj):
+    text = json.dumps(obj, sort_keys = True, indent = 4)
+    return(text)
+
+y = jprint(response.json()['statewise'])
+
+
+### Print message on screen ###
+st.write(y)
+
+### Create slider on screen ###
+x = st.slider('x')
+st.write(x, " squared is", x * x)
 
 
