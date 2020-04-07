0c902f747e9050d537748d4e89be8ace9f5d812a
Rahul Ramachandran <rrahul92.rr@gmail.com>
2020-04-07 23:49:45 +0530

    Further changes

--- Streamlit demo.py
+++ Streamlit demo.py
@@ -1,21 +1,43 @@
 import streamlit as st
 import requests
 import json
+import pandas as pd
+
 response = requests.get("https://api.covid19india.org/data.json")
 print(response.json())
 
-
+@st.cache
 def jprint(obj):
     text = json.dumps(obj, sort_keys = True, indent = 4)
     return(text)
 
-y = jprint(response.json()['statewise'])
-
+df = pd.read_json(jprint(response.json()['statewise']))
 
 ### Print message on screen ###
-st.write(y)
+
+st.title('My first app')
+
+st.write(df)
+
+
+st.markdown("This next word will appear in bold : **_markdown**")
 
 ### Create slider on screen ###
+
+if st.button('say hello'):
+    st.write('helo')
+else:
+    st.write('goodbye')
+
+agree = st.checkbox('I agree')
+
+if agree:
+    st.write('Great!')
+else:
+    st.write('Not Great!')
+    
+    
+
 x = st.slider('x')
 st.write(x, " squared is", x * x)
 
