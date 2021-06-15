7ce6790785bf74ad2ed63be63e80e9a808223128
Rahul Ramachandran <rrahul92.rr@gmail.com>
2021-06-15 11:10:10 +0530

    Adding new file for testing deployment on Heroku

---
+++ model_simulation.py
@@ -0,0 +1,61 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np
+import datetime
+
+#st.set_page_config(page_title = "Marshmallow Car Insurance Simuator", layout = 'wide')
+df_cat = pd.read_csv(r'/users/basingse/desktop/Marshmallow/Risk Pricing Model/Classification Model A/Categories for dashboard input.csv', index_col = None)
+st.title("Marshmallow Car Insurance Simuator")
+st.write("Please answer the following questions to know your premium rate eligibility :")
+
+# Driver related questions
+
+st.subheader('Driver Information : ')
+st.write("#")
+driver_dob = st.date_input("1. Select your date of birth :", datetime.date(1990, 1, 1), min_value = datetime.date(1900, 1, 1))
+st.write("#")
+driver_gender = st.radio("2. What is your gender?", ("Male", "Female"))
+st.write("#")
+driver_marital_status = st.radio("3. What is your Marital status?", ("Unmarried", "Married", "Single", "Others"))
+st.write("#")
+driver_nationality = st.selectbox("4. What is your nationality?", df_cat[~df_cat['nationality'].isna()]['nationality'])
+st.write("#")
+driver_emirates = st.selectbox("5. In which of the following Emirates was your license issued?", df_cat[~df_cat['emirates_issued'].isna()]['emirates_issued'])
+st.write("#")
+
+
+st.write("------------------")
+
+
+# Vehicle related questions
+
+st.subheader('Vehicle Characteristics : ')
+st.write('#')
+vehicle_reg_date = st.date_input("1. Select the first registration date of your vehicle :", datetime.date(1990, 1, 1), min_value = datetime.date(1900, 1, 1))
+st.write('#')
+vehicle_value  = st.number_input("2. What is the value of your vehicle?", value = 10000, min_value = 1000, max_value = 1000000, step = 500)
+st.write('#')
+vehicle_make = st.selectbox("3. What is the make of your vehicle?", df_cat[~df_cat['vehicle_make'].isna()]['vehicle_make']) 
+st.write('#')
+vehicle_reg_type = st.selectbox("4. What is the registration type of your vehicle?", df_cat[~df_cat['vehicle_reg_type'].isna()]['vehicle_reg_type'])
+st.write('#')
+vehicle_body_type = st.selectbox("5. What is the body type of your vehicle?", df_cat[~df_cat['vehicle_body_type'].isna()]['vehicle_body_type'])
+st.write('#')
+vehilce_seats = st.number_input("6. How many seats does your vehicle have?", min_value = 1, value = 4, max_value = 100, step = 1)
+st.write('#')
+vehicle_cylinders = st.number_input("7. How many cylinders does your vehicle have?", min_value = 1, value = 2, max_value = 20, step = 1)
+st.write('#')
+
+# Policy related questions
+
+st.write("------------------")
+st.write("#")
+st.subheader('Policy history : ')
+st.write("#")   
+claim_ever_made = st.radio("1. Have you ever made a vehicle inusrance claim in the past?", ("Yes", "No"))
+st.write("#")
+claim_made_last_year = st.radio("2. Did you make a vehicle insurance claim in the last year?", ("Yes", "No"))
+st.write("#")
+no_claim_years = st.number_input("3. In the past 5 years, how many years have you made NO vehicle insurance claims?", value = 5, min_value = 0, 
+                                    max_value = 5, step = 1)
+st.write("------------------")
